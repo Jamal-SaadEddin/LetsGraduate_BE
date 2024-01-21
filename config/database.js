24a336943929d2b1de8d70ca72aa4a783832a54f
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-01-21 17:17:28 +0200

    Fix issue

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("letsgraduate", "root", "123456", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  "lets_graduate",
+  "lets_grduate_user",
+  "yCQpW1UikjMApHzk",
+  {
+    host: "18.157.127.87",
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;

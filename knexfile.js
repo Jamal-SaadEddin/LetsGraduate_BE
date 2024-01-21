dd684f74071a4ef940c091450707b6a87ee70412
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-01-21 17:00:58 +0200

    Make online database

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,24 @@
+// Update knexfile.js for MySQL
+
 module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "123456",
-      database: "letsgraduate",
+      host: "18.157.127.87", // MySQL server host
+      user: "lets_grduate_user", // MySQL username
+      password: "yCQpW1UikjMApHzk", // MySQL password
+      database: "lets_graduate", // MySQL database name
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
-      directory: "./db/migrations", // You can choose any directory you like
+      tableName: "knex_migrations",
     },
   },
 };
+
 // Commands
 // 1. Running 'npx knex migrate:latest' will execute the up function in the migration, applying the changes to your database.
 // 2. Running 'npx knex migrate:rollback' will execute the down function in the migration, reverting the changes made by the up function.

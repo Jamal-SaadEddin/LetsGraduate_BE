8f5889dd5422d7b790dcf038ab2dceb885ecf48d
OMAR <s11925044@stu.najah.edu>
2024-01-27 13:58:33 +0200

    Edit API that change project status to handle in progress state

--- routes/projects.js
+++ routes/projects.js
@@ -62,19 +62,25 @@ router.put("/projectStatus/:studentId", async (req, res) => {
     const gpState = req.body.gpState;
 
     if (gpState == "in progress") {
-      res.json({ message: "No change on project status" });
-      return;
-    }
-    if (projectType == "GP1") {
+      await Student.update(
+        { gp1State: "in progress", projectType: projectType },
+        { where: { studentId: studentId } }
+      );
+    } else if (projectType == "GP1") {
       await Student.update(
         { gp1State: gpState, projectType: null },
         { where: { studentId: studentId } }
       );
-    } else {
+    } else if (projectType == "GP2") {
       await Student.update(
         { gp2State: gpState, projectType: null },
         { where: { studentId: studentId } }
       );
+    } else {
+      return res.json({
+        message:
+          "Invalid  project type or project State value  please check them again!",
+      });
     }
 
     res.json({ message: "Project status updated successfully" });

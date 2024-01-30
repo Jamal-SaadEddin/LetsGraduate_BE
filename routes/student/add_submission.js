525d6d55180cef8f3714d3873f806254b63f6303
OMAR <s11925044@stu.najah.edu>
2024-01-30 23:27:36 +0200

    Implement add submission API
    Edit it if the group submitted before, don't create another submission instead of this we update the old submission

--- routes/student/add_submission.js
+++ routes/student/add_submission.js
@@ -11,19 +11,86 @@ router.post("/submission", async (req, res) => {
       acceptStatus: "Pending",
     };
 
-    // create new user based on data passed by POST body
-    const newSubmission2 = await Submission.create(newSubmission);
-
-    const submission = await Submission.findOne({
+    // check if group is submitted before
+    const checkSubmission = await Submission.findOne({
       where: {
         projectId: newSubmission.projectId,
       },
     });
 
-    res.json(submission);
+    if (checkSubmission) {
+      await Submission.update(
+        { file: newSubmission.file },
+        {
+          where: {
+            projectId: newSubmission.projectId,
+          },
+          fields: ["file"],
+        }
+      );
+
+      const editedSubmission = await Submission.findOne({
+        where: {
+          projectId: newSubmission.projectId,
+        },
+      });
+      res.json(editedSubmission);
+    } else {
+      // create new submission based on data passed by POST body
+      const newSubmission2 = await Submission.create(newSubmission);
+
+      const submission = await Submission.findOne({
+        where: {
+          projectId: newSubmission.projectId,
+        },
+      });
+      res.json(submission);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error adding submission" });
+  }
+});
+
+router.delete("/submission", async (req, res) => {
+  try {
+    const { userId } = req.query;
+
+    // Fetch user type
+    const userType = await User.findOne({
+      attributes: ["type"],
+      where: {
+        userId: userId,
+      },
+    });
+
+    // delete user
+    await Submission.destroy({
+      where: {
+        userId: userId,
+      },
+    });
+
+    if (userType.type === "student") {
+      //delete student data
+      await Student.destroy({
+        where: {
+          studentId: userId,
+        },
+      });
+    } else {
+      //delete student data
+      await Doctor.destroy({
+        where: {
+          doctorId: userId,
+        },
+      });
+    }
+
+    res.json({ message: "Account data deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error in deleting account data" });
   }
 });
 

cdc35d450212f44dd1a3869317f6157d33362b6f
OMAR <s11925044@stu.najah.edu>
2024-02-06 23:19:27 +0200

    Edit verify email API
    Make sending verification code in separate function and fix some errors

---
+++ functions/send_verification_code.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+async function sendVerificationCode(email, code) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "s11925044@stu.najah.edu",
+      pass: "----",
+    },
+  });
+
+  // create mailOptions
+  const mailOptions = {
+    from: "s11925044@stu.najah.edu",
+    to: email,
+    subject: "Let's Graduate Email Verification",
+    text: `Your verification code is: : ${code}.`,
+  };
+
+  try {
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({
+      message: "email not found",
+      body: req.body,
+    });
+  }
+}
+
+module.exports = sendVerificationCode;
--- routes/authentication/verify_email.js
+++ routes/authentication/verify_email.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const randomstring = require("randomstring");
 const User = require("../../models/user");
+const sendVerificationCode = require("../../functions/send_verification_code");
 require("dotenv").config();
-const nodemailer = require("nodemailer");
 
 router.put("/email", async (req, res) => {
   try {
@@ -29,51 +29,16 @@ router.put("/email", async (req, res) => {
         }
       );
 
-      // Create secure transport
-      const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: process.env.EMAIL_SECURE === "true",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+      await sendVerificationCode(email, verificationCode);
 
-      // create mailOptions
-      const mailOptions = {
-        from: process.env.EMAIL_FROM, // Replace with actual sender address (e.g., "noreply@yourdomain.com")
-        to: "omarmustafaqaneer@gmail.com",
-        subject: "Email Verification",
-        text: `Hello,
-      
-        Please verify your email address by entering the following code: ${verificationCode}
-      
-        This code is valid for 1 hour.
-      
-        Thank you for registering!
-      
-        Your app name`,
-      };
-
-      // send email
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email:", error);
-          // Handle email sending errors gracefully (e.g., notify admin, retry later)
-        } else {
-          console.log("Email sent:", info.response);
-          // Handle successful email sending (e.g., log in database, display confirmation message)
-        }
-      });
-
+      //omarmustafaqaneer@gmail.com
       res.json({ message: "verification code updated successfully" });
     } else {
       res.json({ message: "Invalid email please check it and try again" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error fetching abstract data" });
+    res.status(500).json({ message: "Error sending email" });
   }
 });
 
